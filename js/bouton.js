1576866f52ff6edbead6903e5ea23fdc867b571e
Prospere <prospere.lucel@gmail.Com>
2020-10-24 23:26:49 +0200

    Autoreconnect

--- js/bouton.js
+++ js/bouton.js
@@ -1,3 +1,4 @@
+
 $(function() { //shorthand document.ready function
 
 	$('#BOUTTON01').on('submit', function(e) { //use on if jQuery 1.7+
@@ -7,15 +8,38 @@ $(function() { //shorthand document.ready function
     });	
 	
 	$('#BOUTTON02').on('submit', function(e) { //use on if jQuery 1.7+
-	e.preventDefault(); //prevent form from submitting
-	var data = $("#BOUTTON02").serializeArray();
-	SendCommands2();
+		e.preventDefault(); //prevent form from submitting
+		var data = $("#BOUTTON02").serializeArray();
+		SendCommands2();
 	});	
 
 	
 });
 
-
+const obs = new OBSWebSocket()
+let obsConnected = false
+let reconnectInterval = null
+function ConnectOBS() {
+  obs.connect({
+	address: '192.168.1.10:4444',
+	password: 'password'
+  });
+}
+obs.on('ConnectionOpened', () => {
+  clearInterval(reconnectInterval)
+  reconnectInterval = null
+  obsConnected = true
+})
+obs.on('ConnectionClosed', () => {
+  obsConnected = false
+  reconnectInterval = setInterval(() => {
+	if (!obsConnected) {
+	  ConnectOBS()
+	}
+  }, 5000);
+})
+ConnectOBS();
+	
 function SendCommands1() {
 	setTimeout(function(){
 		obs.send('SetSceneItemRender', {
@@ -44,8 +68,8 @@ function SendCommands2() {
 	}, 0);
 	setTimeout(function(){
 		obs.send('SetSceneItemRender', {
-			'scene-name': 'Source_Name2',
-			'source': 'caca',
+			'scene-name': 'TEST_SCENE',
+			'source': 'Source_Name2',
 			'render': false
 		})
 	}, 5000);
